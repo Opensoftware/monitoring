1b91c71b380a34cfa1c0c626ac60238fb36b70cd
blelump <blelump@martex.nom.pl>
2015-10-22 09:58:04 +0000

    Graphite image dockerfile

---
+++ build/graphite_web/data/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'NAME': '/data/graphite/storage/graphite.db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
+    }
+}
---
+++ build/graphite_web/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'graphiteweb' ]; then
+  exec /usr/sbin/apache2ctl -D FOREGROUND
+fi
+
+exec "$@"
+
